6bae927019fbe4c5495be6fdbb88e56ec602de9a
aidan.newell <acnewell09@gmail.com>
2023-05-30 07:48:50 -0500

    making search bar UI

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="seach-bar-container">
+      <div className="search-bar-container">
         <div>Searchbar</div>
         <div>Search Results</div>
       </div>
