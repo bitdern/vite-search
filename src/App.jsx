6076080d6a55635ac6351b5302c68acb7f233a65
aidan.newell <acnewell09@gmail.com>
2023-05-31 07:57:18 -0500

    added styles to the search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
+
 import "./App.css";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
     <div className="App">
       <div className="search-bar-container">
-        <div>Searchbar</div>
+        <SearchBar />
         <div>Search Results</div>
       </div>
     </div>
