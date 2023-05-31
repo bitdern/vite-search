750ec04d7f2be93bb28636f12e503d2fd0aab996
aidan.newell <acnewell09@gmail.com>
2023-05-31 08:04:55 -0500

    search functionality

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { FaSearch } from "react-icons/fa";
 import "./SearchBar.css";
 
 const SearchBar = () => {
+  const [input, setInput] = useState("");
+
+  const fetchData = (value) => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+      });
+  };
+
+  const handleChange = (value) => {
+    setInput(value);
+    fetchData(value);
+  };
+
   return (
     <div className="input-wrapper">
       <FaSearch id="search-icon" />
-      <input placeholder="Type something here..." />
+      <input
+        placeholder="Type something here..."
+        value={input}
+        onChange={(e) => handleChange(e.target.value)}
+      />
     </div>
   );
 };
